fb24d40fc49969c7742d0f2d7bfe2358ac2cb662
August Conwell <august.conwell@gmail.com>
2017-05-18 22:32:33 -0400

    Added views and styled landing page

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,5 +1,11 @@
 module.exports = function (app) { 
-    app.get('/', function (req, res) { 
-        res.render('index')
-    })
+    app.get('/', function (req, res) {
+        res.render('index', { style: 'landing' })
+    });
+
+    app.get('/profile', function (req, res) { 
+        res.render('profile', {
+            style: 'profile'
+        });
+    });
 }
