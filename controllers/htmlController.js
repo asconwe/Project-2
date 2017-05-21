ece69940325199885aa1724e875186eb02b95048
August Conwell <august.conwell@gmail.com>
2017-05-21 09:23:43 -0400

    Divided html controller into smaller files

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,11 +1,26 @@
-module.exports = function (app) { 
+var helper = require('utilities/helper.js');
+
+module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index', { style: 'landing' })
     });
 
-    app.get('/profile', function (req, res) { 
-        res.render('profile', {
-            style: 'profile'
-        });
+    app.get('/profile', function (req, res) {
+        // /profile?username=asdl
+        var username = req.query.username
+        var profile = helper.getUserProfile(username)
+        if (helper.hasAccount(username)) {
+            res.render('profile', {
+                style: 'profile',
+                profile: profile
+            });
+        }
+    });
+
+    app.get('/:activity', function (req, res) {
+
+        activity = helper.getActivity(req.query.activity) {
+
+        }
     });
 }
