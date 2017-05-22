463bfb39d41a91881e1eddadd90f43a3a40f24f3
August Conwell <august.conwell@gmail.com>
2017-05-22 00:08:14 -0400

    Beginning of profile page, tweaked landing page

--- controllers/htmlProfile.js
+++ controllers/htmlProfile.js
@@ -1,12 +1,31 @@
-function getUserProfile() { 
-    
+function getUserProfile(username, callback) {
+    console.log('in getUser')
+    // sequelize query here
+    var profile = {
+        username: username,
+        activity: [
+            {
+                itemName: 'Go skydiving',
+                location: 'Virginia',
+                complete: false
+            },
+            {
+                itemName: 'Go Bungee Jumping',
+                location: 'Virginia',
+                complete: true
+            }
+        ]
+    } // this would come fromt he sequelize query
+    callback(profile);
 }
 
 module.exports = function (app) {
     app.get('/profile', function (req, res) {
+        console.log('in get Router')
         // /profile?username=asdl
         var username = req.query.username;
-        getUserProfile(username, function (profile) { 
+        getUserProfile(username, function (profile) {
+            console.log('in callback')
             res.render('profile', {
                 style: 'profile',
                 profile: profile
