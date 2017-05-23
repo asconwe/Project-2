cfdb246bada567e61430d5f6953734319d7cdfe1
August Conwell <august.conwell@gmail.com>
2017-05-23 00:01:38 -0400

    username authentication works

--- controllers/htmlProfile.js
+++ controllers/htmlProfile.js
@@ -1,22 +1,26 @@
+var db = require("../models");
+
 function getUserProfile(username, callback) {
     console.log('in getUser')
     // sequelize query here
-    var profile = {
-        username: username,
-        activity: [
-            {
-                itemName: 'Go skydiving',
-                location: 'Virginia',
-                complete: false
-            },
-            {
-                itemName: 'Go Bungee Jumping',
-                location: 'Virginia',
-                complete: true
-            }
-        ]
-    } // this would come fromt he sequelize query
-    callback(profile);
+    db.Person.findOne({ where: { username: username }, include: [db.Activity] }).then(function (profile) { 
+        callback(profile);
+    })
+    // var profile = {
+    //     username: username,
+    //     activity: [
+    //         {
+    //             itemName: 'Go skydiving',
+    //             location: 'Virginia',
+    //             complete: false
+    //         },
+    //         {
+    //             itemName: 'Go Bungee Jumping',
+    //             location: 'Virginia',
+    //             complete: true
+    //         }
+    //     ]
+    // } // this would come fromt he sequelize query
 }
 
 module.exports = function (app) {
