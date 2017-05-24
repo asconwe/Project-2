00916f4c88884f576c559918f8f86dea0a8c58b3
August Conwell <august.conwell@gmail.com>
2017-05-23 22:12:20 -0400

    Layout tweaks

--- public/javascript/modal.js
+++ public/javascript/modal.js
@@ -1,27 +1,27 @@
 //Open modal
-$(document).ready(function(){
-$("#sign-up-button").click(function () {
-  $(".modal").css('display', 'block');
-});
+$(document).ready(function () {
+  $("#modal-button").click(function () {
+    $("#modal").css('display', 'block');
+  });
 
-//Close modal
-$("#close").click(function () {
-  $("#signupmod").css('display', 'none');
-});
+  //Close modal
+  $("#close").click(function () {
+    $("#modal").css('display', 'none');
+  });
 
-//enter data into tables
-$("#signupform").submit(function(e){
-  e.preventDefault();
-  var userEmail=$("#userEmail").val().trim();
-  var fullName=$("#fullName").val().trim();
-  var username=$("#username").val().trim();
-  console.log(userEmail);
-  console.log(fullName);
-  console.log(username);
+  //enter data into tables
+  $("#signupform").submit(function (e) {
+    e.preventDefault();
+    var userEmail = $("#userEmail").val().trim();
+    var fullName = $("#fullName").val().trim();
+    var username = $("#username").val().trim();
+    console.log(userEmail);
+    console.log(fullName);
+    console.log(username);
 
-  $.post("/api/user/new", {userEmail:userEmail, fullName:fullName, username:username}, function(result){
+    $.post("/api/user/new", { userEmail: userEmail, fullName: fullName, username: username }, function (result) {
       console.log(result);
     });
-});
+  });
 
 });
