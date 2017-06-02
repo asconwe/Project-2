34c1bdbe63ca838104dfa996276a63ed79314066
August Conwell <august.conwell@gmail.com>
2017-06-02 00:13:09 -0400

    Added comments to controllers

--- controllers/apiEditActivity.js
+++ controllers/apiEditActivity.js
@@ -1,17 +1,21 @@
 var db = require("../models");
 module.exports = function (app) {
+	//================= Listen for a get request and return the activity information
 	app.get("/api/activity/edit/:id", function (req, res) {
 		db.Activity.findOne({ where: { ActivityId: req.params.id } }).then(function (dbActivity) { 
 			res.json(dbActivity);
 		})
 	})
+	// The information from the first request populates on the client's text fields
+	// Then the user edits the information and clicks submit
+	//================= Listen for the updated information
 	app.put("/api/activity/edit/:id", function (req, res) {
-		console.log('now here====================', req.body);
 		db.JoinedActivity.update({ itemName: req.body.itemName, tags: req.body.tags, location: req.body.location, description: req.body.description, complete: false }, {
 			where: {
 				ActivityId: req.params.id
 			}
 		}).then(function (Activity) {
+			//================= Return user to their profile where updated activity information will populate
 			res.redirect('/profile?key=id&val=' + req.body.PersonId);
 		});
 	});
--- controllers/apiNewActivity.js
+++ controllers/apiNewActivity.js
@@ -1,15 +1,15 @@
 var db = require("../models");
 
 module.exports = function (app) {
+  //================= Listen for new activity posts
   app.post("/api/activity/new", function (req, res) {
-    console.log('now here====================', req.body);
     var activity = req.body;
     activity.complete = false;
     db.Activity.create(activity).then(function (dbActivity) {
-      console.log(dbActivity.id);
+      // associate the tags and any joined users
       db.TagActivity.create({ ActivityId: dbActivity.id, TagId: req.body.TagId });
       db.JoinedActivity.create({ ActivityId: dbActivity.id, PersonId: req.body.PersonId }).then(function (userAndActivity) {
-        // res.json(dbActivity);
+        //================= Return the user to their profile with the new activity added
         res.redirect('/profile?key=id&val=' + req.body.PersonId);
       });
     });
